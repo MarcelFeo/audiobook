3eb1059d3a9132b1260470378cb115cb91160e31
MarcelFeo <marcelfeo29@gmail.com>
2022-04-29 12:47:48 -0300

    finalizando a integração OCR

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const video = document.querySelector("video");
 async function setUp() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true });
     video.srcObject = stream;
+
+    video.addEventListener("playing", async () => {
+        const worker = Tesseract.createWorker();
+        await worker.load();
+        await worker.loadLanguage("por");
+        await worker.initialize("por");
+
+        const canvas = document.createElement("canvas");
+        canvas.width = video.width;
+        canvas.height = video.height;
+
+        document.addEventListener("keypress", async e => {
+            if (e.code !== "Space") return
+            
+            canvas.getContext("2d").drawImage(video, 0, 0, video.width, video.height);
+            const { data } = await worker.recognize(canvas);
+            console.log(data);
+        })
+    })
 }
 
 setUp();
